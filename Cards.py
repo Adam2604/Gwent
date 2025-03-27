3c043eb782221c6f7b0c9b2a2512ec6a5a8a15bb
Adam2604 <adammichala11@gmail.com>
2025-03-27 12:06:30 +0100

    Dodanie pliku "Classes" i podmiana zawartości

--- Cards.py
+++ Cards.py
@@ -1,49 +1,55 @@
+from Classes import *
 
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.cards_in_the_deck = []
-        self.cards_in_the_game = []
+#zbiór kart pogodowych
+weather_cards = [
+    Special_card("Czyste niebo", clear)
+    Special_card("Czyste niebo", clear)
+    Special_card("Ulewny deszcz", one_point)
+    Special_card("Ulewny deszcz", one_point)
+    Special_card("Gęsta mgła", one_point)
+    Special_card("Gęsta mgła", one_point)
+    Special_card("Gęsta mgła", one_point)
+    Special_card("Trzaskający mróz", one_point)
+    Special_card("Trzaskający mróz", one_point)
+    Special_card("Trzaskający mróz", one_point)
+]
 
-    def use_card(self, card):
-        self.cards_in_the_deck.append(card)
-        print(f"{self.name} used {card.name} (Strength: {card.strength})")
-
-class Fraction:
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return f"Fraction: {self.name}"
-
-class Unit:
-    def __init__(self, name, strength, fraction, type,  effect = None):
-        self.name = name
-        self.strength = strength
-        self.fraction = fraction
-        self.type = type
-        self.effect = effect
-
-    def __str__(self):
-        return f"{self.name} {self.strength} {self.fraction} {self.effect}"
-
-class Close_combat(Unit):
-    def __init__(self, name, strength, fraction, effect = None):
-        super().__init__(name, strength, fraction, "Close_combat", effect)
-
-
-class Ranged_combat(Unit):
-    def __init__(self, name, strength, fraction, effect = None):
-        super().__init__(name, strength, fraction, "Ranged_combat", effect)
-
-
-class Siege_combat(Unit):
-    def __init__(self, name, strength, fraction, effect = None):
-        super().__init__(name, strength, fraction, "Siege_combat", effect)
-
-
-class Special_card(Unit):
-    def __init__(self, name, effect):
-        super().__init__(name, None, None, "Special_card", effect)
-
-
+#zbiór kart Królestwa Północy
+Northern_Realms_cards = [
+    Siege_combat("Mistrz Oblężeń z Kaedwen", 1, "Królestwo Północy"),  # efekt +1 dla wszystkich
+    Siege_combat("Mistrz Oblężeń z Kaedwen", 1, "Królestwo Północy"),  # efekt +1 dla wszystkich
+    Siege_combat("Mistrz Oblężeń z Kaedwen", 1, "Królestwo Północy"),  # efekt +1 dla wszystkich
+    Close_combat("Redański Piechur", 1, "Królestwo Północy"),
+    Close_combat("Redański Piechur", 1, "Królestwo Północy"),
+    Close_combat("Biedna Pierdolona Piechota", 1, "Królestwo Północy"),  # efekt - połączone podwajają punkty swoje
+    Close_combat("Biedna Pierdolona Piechota", 1, "Królestwo Północy"),
+    Close_combat("Biedna Pierdolona Piechota", 1, "Królestwo Północy"),
+    Close_combat("Talar", 1, "Królestwo Północy"),  # szpieg
+    Close_combat("Yarpen Zirgin", 2, "Królestwo Północy"),
+    Close_combat("Komandos Niebieskich Pasów", 4, "Królestwo Północy"),  # połączone podwajają punkty swoje
+    Close_combat("Komandos Niebieskich Pasów", 4, "Królestwo Północy"),
+    Close_combat("Komandos Niebieskich Pasów", 4, "Królestwo Północy"),
+    Ranged_combat("Sheldon Skaggs", 4, "Królestwo Północy"),
+    Ranged_combat("Sabrina Glevissig", 4, "Królestwo Północy"),
+    Close_combat("Sigismund Dijkstra", 4, "Królestwo Północy"),  # szpieg
+    Siege_combat("Medyczka Burej Chorągwi", 5, "Królestwo Północy"),  # przywraca kartę ze stosu odrzuconych
+    Ranged_combat("Rębacze z Crifrid", 5, "Królestwo Północy"),  # połączone podwajają punkty swoje
+    Ranged_combat("Rębacze z Crifrid", 5, "Królestwo Północy"),
+    Ranged_combat("Rębacze z Crifrid", 5, "Królestwo Północy"),
+    Close_combat("Książe Stannis", 5, "Królestwo Północy"),  # szpieg
+    Ranged_combat("Sheala the Tancarville", 5, "Królestwo Północy"),
+    Ranged_combat("Keira Metz", 5, "Królestwo Północy"),
+    Close_combat("Zygfryd z Denesle", 5, "Królestwo Północy"),
+    Close_combat("Ves", 5, "Królestwo Północy"),
+    Siege_combat("Wieża oblężnicza", 6, "Królestwo Północy"),
+    Siege_combat("Balista", 6, "Królestwo Północy"),
+    Siege_combat("Trebusz", 6, "Królestwo Północy"),
+    Siege_combat("Trebusz", 6, "Królestwo Północy"),
+    Ranged_combat("Detmold", 6, "Królestwo Północy"),
+    Siege_combat("Katapulta", 8, "Królestwo Północy"),  # połączone podwajają punkty swoje
+    Siege_combat("Katapulta", 8, "Królestwo Północy"),
+    Ranged_combat("Philippa Eilhart", 10, "Królestwo Północy"),  # bohater
+    Close_combat("Esterad Thyssen", 10, "Królestwo Północy"),  # bohater
+    Close_combat("Jan Natalis", 10, "Królestwo Północy"),  # bohater
+    Close_combat("Vernon Roche", 10, "Królestwo Północy")  # bohater
+]
---
+++ Classes.py
@@ -0,0 +1,47 @@
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.cards_in_the_deck = []
+        self.cards_in_the_game = []
+
+    def use_card(self, card):
+        self.cards_in_the_deck.append(card)
+        print(f"{self.name} used {card.name} (Strength: {card.strength})")
+
+class Fraction:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"Fraction: {self.name}"
+
+class Unit:
+    def __init__(self, name, strength, fraction, type,  effect = None):
+        self.name = name
+        self.strength = strength
+        self.fraction = fraction
+        self.type = type
+        self.effect = effect
+
+    def __str__(self):
+        return f"{self.name} {self.strength} {self.fraction} {self.effect}"
+
+class Close_combat(Unit):
+    def __init__(self, name, strength, fraction, effect = None):
+        super().__init__(name, strength, fraction, "Close_combat", effect)
+
+
+class Ranged_combat(Unit):
+    def __init__(self, name, strength, fraction, effect = None):
+        super().__init__(name, strength, fraction, "Ranged_combat", effect)
+
+
+class Siege_combat(Unit):
+    def __init__(self, name, strength, fraction, effect = None):
+        super().__init__(name, strength, fraction, "Siege_combat", effect)
+
+
+class Special_card(Unit):
+    def __init__(self, name, effect):
+        super().__init__(name, None, None, "Special_card", effect)
